e7e1c8d11da65030a8dbe2cb70ffc0028b0fde3f
AndriiSkliar <89527037+AndriiSkliar@users.noreply.github.com>
2024-05-16 10:59:22 +0000

    Deploying to gh-pages from @ AndriiSkliar/eliftech-react@07c3948f32faed99b5d505d5c12fe28b7eeb80d1 🚀

---
+++ assets/ParticipantsPage-fba56b8c.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-67134324.js";const a=()=>t.jsx("div",{children:t.jsx("h1",{children:"Participants page"})});export{a as default};
---
+++ assets/RegisterPage-5e5010f6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-67134324.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"Register page"})});export{s as default};
