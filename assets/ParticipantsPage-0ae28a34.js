a108543ac704416b307b1a2668a80a9847b37dbf
AndriiSkliar <89527037+AndriiSkliar@users.noreply.github.com>
2024-05-21 08:03:11 +0000

    Deploying to gh-pages from @ AndriiSkliar/eliftech-react@3ab80dac372013b4180ced2f538c013aaa56382c 🚀

---
+++ assets/ParticipantsPage-0ae28a34.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-738aa2f7.js";import{T as s}from"./Title-3b7f0258.js";import{P as i}from"./ParticipantsList-61d87e49.js";const o=()=>t.jsxs("div",{children:[t.jsx(s,{title:'"Awesome events" participants'}),t.jsx(i,{})]});export{o as default};
